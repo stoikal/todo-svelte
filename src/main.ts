e0cbafce4201c731215d95a5a93dd95579f4dab6
Laksito Pamilih <laksito@protonmail.com>
2023-06-22 16:11:44 +0700

    set up unocss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import App from './App.svelte'
+import 'virtual:uno.css'
+import '@unocss/reset/tailwind-compat.css'
 
 const app = new App({
   target: document.getElementById('app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import UnoCSS from 'unocss/vite'
+import extractorSvelte from '@unocss/extractor-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    UnoCSS({
+      extractors: [
+        extractorSvelte()
+      ]
+    }),
     svelte()
   ]
 })
