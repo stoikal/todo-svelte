47d490573787d210bd157072542beb0774e195af
Laksito Pamilih <laksito@protonmail.com>
2023-06-22 16:35:55 +0700

    add deploy github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import UnoCSS from 'unocss/vite'
 import extractorSvelte from '@unocss/extractor-svelte'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${pkg.name}/`,
   plugins: [
     UnoCSS({
       extractors: [
